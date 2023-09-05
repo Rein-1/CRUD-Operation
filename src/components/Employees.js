935f28ac239ae594906701dfaf5fe7f26309213a
Rein-1 <markreniermercado123@gmail.com>
2023-09-05 22:34:55 +0800

    Save

---
+++ src/components/Employees.js
@@ -0,0 +1,19 @@
+const Employess = [
+    {
+        id:"1",
+        Name:"Mark Mercado",
+        Age:"25"
+    },
+    {
+        id:"2",
+        Name:"Juan Two Three",
+        Age:"20"
+    },
+    {
+        id:"3",
+        Name:"John Wick",
+        Age:"34"
+    }
+]
+
+export default Employess;
